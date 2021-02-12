f8c8d73f995ed1921cec0b00d239b9d0c7090e9d
sociallyvirtual7 <75465980+sociallyvirtual7@users.noreply.github.com>
2021-02-12 16:43:28 +0530

    Add files via upload

---
+++ ExportVIPermissions.ps1
@@ -0,0 +1,19 @@
+﻿Set-PowerCLIConfiguration -InvalidCertificateAction Ignore -Confirm:$false
+$cred = Get-Credential
+Connect-VIServer vCenter_IP/FQDN -Credential $cred
+$vms = Get-VM
+$output = New-Object System.Collections.ArrayList($null)
+$vmpermission = @()
+foreach ($vm in $vms)
+{
+    $permissions = $vm | Get-VIPermission | Get-Unique
+    Foreach ($permission in $permissions)
+    {
+        $formatpermission = $permission.Principal + " (" + $permission.Role + ")"
+        $permission | Add-Member -NotePropertyName FormatPermission -NotePropertyValue $formatpermission
+    }
+    $vmpermission = [pscustomobject]@{VMName=$vm.Name;Role=$permissions.FormatPermission  -join ', '}
+    [void]($output.Add($vmpermission))
+}
+ 
+$output | Export-csv C:\Users\VMPermissionsExport.csv -NoTypeInformation
