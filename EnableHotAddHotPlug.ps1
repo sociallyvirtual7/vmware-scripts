7633607d8ed9bd4ebaa2c46bd553055fd3db981c
sociallyvirtual7 <75465980+sociallyvirtual7@users.noreply.github.com>
2022-09-11 16:48:02 +0530

    Add files via upload

---
+++ EnableHotAddHotPlug.ps1
@@ -0,0 +1,16 @@
+﻿Connect-VIServer vCenter_Hostname -Credential (Get-Credential)
+
+$vms = Get-VM | where {($_.PowerState -eq "PoweredOff") -and ($_.ExtensionData.Summary.Runtime.ConnectionState -eq "connected")}
+
+$spec = New-Object VMware.Vim.VirtualMachineConfigSpec
+$spec.CpuHotAddEnabled = $true
+$spec.MemoryHotAddEnabled = $true
+
+foreach ($vm in $vms)
+{
+
+    if(-not $vm.ExtensionData.Config.cpuHotAddEnabled -or -not $vm.ExtensionData.Config.memoryHotAddEnabled){
+    $vm.ExtensionData.ReconfigVM($spec)
+    }
+}
+Disconnect-VIServer -Confirm:$false
