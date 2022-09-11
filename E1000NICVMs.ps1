0dd7dde1e93f2148584f538ae69bf3283775b0fa
sociallyvirtual7 <75465980+sociallyvirtual7@users.noreply.github.com>
2022-09-11 16:48:47 +0530

    Add files via upload

---
+++ E1000NICVMs.ps1
@@ -0,0 +1,2 @@
+﻿$vms = ForEach( $VM in (Get-Cluster "Cluster_Name" | Get-VM) ) { $VM|Where{ $VM|Get-NetworkAdapter|Where{ $_.Type -like "*e1000*" -or $_.Type -like "*Flexible*"} } }
+$vms | Sort-Object "Name"
